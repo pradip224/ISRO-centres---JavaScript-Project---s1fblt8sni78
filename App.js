59f709d62c482607828b32d78a6ac59fc26d0d86
pradip patra <pradip.patra224@gmail.com>
2023-03-27 16:32:26 +0530

    structured js

--- App.js
+++ App.js
@@ -1,97 +1,145 @@
+//getting reference to all the html components
+const searchInput = document.getElementsByClassName("js-input")[0];
+const searchBtn = document.getElementsByClassName("js-search-btn")[0];
+const cityBtn = document.getElementsByClassName("js-city-btn")[0];
+const stateBtn = document.getElementsByClassName("js-state-btn")[0];
+const centerBtn = document.getElementsByClassName("js-center-btn")[0];
+const cardContainer = document.getElementsByClassName("js-card-container")[0];
 
-const searchInput = document.getElementsByClassName('js-input')[0];
-const searchBtn = document.getElementsByClassName('js-search-btn')[0];
-const cityBtn = document.getElementsByClassName('js-city-btn')[0];
-const stateBtn = document.getElementsByClassName('js-state-btn')[0];
-const centerBtn = document.getElementsByClassName('js-center-btn')[0];
-const cardContainer = document.querySelector('.card-container');
+//search filter by state ,name or place
+let selectedFilter;
+//jsonData holds the data fetched from the API
+let jsonData;
 
-let selectedFilter = 'all';
-let jsonData ;
-
-function getCenters() {
-  fetch('https://isro.vercel.app/api/centres')
-    .then(response => response.json())
-    .then(data => {
-      jsonData = data.centres;
-      renderCenters(data.centres);
-    });
+//fetching data from the API
+async function getCenters() {
+  try {
+    const response = await fetch("https://isro.vercel.app/api/centres");
+    const data = await response.json();
+    jsonData = data.centres;
+    renderCenters(jsonData);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+//rnders the card components inside the card-container
+function renderCenters(centers, selectedFilter = "all") {
+  cardContainer.innerHTML = "";
+  const fragment = document.createDocumentFragment();
 
-
-function renderCenters(centers) {
-
-  cardContainer.innerHTML = '';
-  let htmlString;
-  
-  centers.forEach(center => {
-    if (selectedFilter === 'all' || center[selectedFilter].toLowerCase().includes(searchInput.value.toLowerCase())) {
-
-    //   const card = document.createElement('div');
-    //   card.classList.add('center-card');
+  //traverse through the array of objects and creates the card components as per the matched search string
+  centers.forEach((center) => {
+    if (
+      selectedFilter === "all" ||
+      center[selectedFilter]
+        .toLowerCase()
+        .includes(searchInput.value.toLowerCase())
+    ) {
       
-    //   const name = document.createElement('h2');
-    //   name.textContent = center.name;
-      
-    //   const location = document.createElement('p');
-    //   location.textContent = `${center.Place}, ${center.State}`;
-      
-      htmlString = `
-                <div class ="card parent">
-                    <div class ="w-60">
-                        <div>CENTER</div>
-                        <div class ="t-overflow">${center.name}</div>
-                    </div>
-                    <div class ="w-20">
-                        <div>CITY</div>
-                        <div class ="t-overflow">${center.Place}</div>
-                    </div>
-                    <div class ="w-20">
-                        <div>STATE</div>
-                        <div class ="t-overflow">${center.State}</div>
-                    </div>
-                    
-                </div>
-      `
-      
-    //   card.appendChild(name);
-    //   card.appendChild(location);
-    const div = document.createElement('div');
-    div.innerHTML = htmlString;
-      
-    cardContainer.appendChild( div);
-    }    
+      const card = createCardElement(center.name, center.Place, center.State);
+      fragment.appendChild(card);
+    }
   });
-
+  //renders the card components onto the card-container DOM 
+  cardContainer.appendChild(fragment);
 }
 
-searchBtn.addEventListener('click', () => {
-  renderCenters(jsonData);
-});
+//creates card component with the name,place and state  and returns the card coponent
+function createCardElement(name, place, state) {
+  //parent div container
+  const centerElement = document.createElement("div");
+  centerElement.classList.add("card", "parent", "d-flex", "flex-sp-between");
 
-cityBtn.addEventListener('click', () => {
-  selectedFilter = 'Place';
-  cityBtn.classList.add('active')
-  stateBtn.classList.remove('active')
-  centerBtn.classList.remove('active');
-});
+//CENTER div container
+  const w60 = document.createElement("div");
+  w60.classList.add("w-60");
 
-stateBtn.addEventListener('click', () => {
-  selectedFilter = 'State';
-  stateBtn.classList.add('active')
-  cityBtn.classList.remove('active')
-  centerBtn.classList.remove('active');
-});
+  const centerLabel = document.createElement("div");
+  centerLabel.innerText = "CENTER";
+  w60.appendChild(centerLabel);
 
-centerBtn.addEventListener('click', () => {
-  selectedFilter = 'name';
-  centerBtn.classList.add('active');
-  stateBtn.classList.remove('active')
-  cityBtn.classList.remove('active')
-});
+  const centerName = document.createElement("div");
+  centerName.classList.add("t-overflow");
+  centerName.innerText = name;
+  w60.appendChild(centerName);
 
-// getCenters();
+  //CITY div container
+  const w20City = document.createElement("div");
+  w20City.classList.add("w-20");
 
-document.addEventListener('DOMContentLoaded',getCenters);
+  const cityLabel = document.createElement("div");
+  cityLabel.innerText = "CITY";
+  w20City.appendChild(cityLabel);
 
+  const cityName = document.createElement("div");
+  cityName.classList.add("t-overflow");
+  cityName.innerText = place;
+  w20City.appendChild(cityName);
+
+// STATE div container
+  const w20State = document.createElement("div");
+  w20State.classList.add("w-20");
+
+  const stateLabel = document.createElement("div");
+  stateLabel.innerText = "STATE";
+  w20State.appendChild(stateLabel);
+
+  const stateName = document.createElement("div");
+  stateName.classList.add("t-overflow");
+  stateName.innerText = state;
+  w20State.appendChild(stateName);
+
+  //appending name,city and state onto the parent div
+  centerElement.appendChild(w60);
+  centerElement.appendChild(w20City);
+  centerElement.appendChild(w20State);
+
+  return centerElement;
+}
+
+//handles search button click
+function handleSearchBtnClick() {
+  renderCenters(jsonData, selectedFilter);
+}
+
+//handles city button click
+function handleCityBtnClick() {
+  selectedFilter = "Place";
+  cityBtn.classList.add("active");
+  stateBtn.classList.remove("active");
+  centerBtn.classList.remove("active");
+}
+
+//handles state button click
+function handleStateBtnClick() {
+  selectedFilter = "State";
+  stateBtn.classList.add("active");
+  cityBtn.classList.remove("active");
+  centerBtn.classList.remove("active");
+}
+
+//handles center button click
+function handleCenterBtnClick() {
+  selectedFilter = "name";
+  centerBtn.classList.add("active");
+  stateBtn.classList.remove("active");
+  cityBtn.classList.remove("active");
+}
+
+//binds events to the buttons
+function bindEvents() {
+  searchBtn.addEventListener("click", handleSearchBtnClick);
+  cityBtn.addEventListener("click", handleCityBtnClick);
+  stateBtn.addEventListener("click", handleStateBtnClick);
+  centerBtn.addEventListener("click", handleCenterBtnClick);
+}
+
+//main function
+function main() {
+  bindEvents();
+  getCenters();
+}
+
+//invokes main when DOM Contents Loaded
+document.addEventListener("DOMContentLoaded", main);
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # html-css-js-project-boilerplate
+
+
+## Project objective
+
+- You will be building an ISRO research centres  finder project using HTML, CSS, and JavaScript.
+- In the end, we will have a frontend application in which you can type a city name, state name or an observatory name and get the names of centres of ISRO with their city name and state they are in .
+- The API we are going to use in this project is [`https://isro.vercel.app/api/centres`](https://isro.vercel.app/api/centres)
+
+## **Project Context**
+
+- We all know about our Space agency of our country, but many few know about how many centres it has in  different cities with their names.
+- The reason for the creation of this website is to make a platform where people can search their city name or state name and get the centres of ISRO in that city or state.
+- One can also search for the ISRO observatory they know to get it located state and city.
+
+**Tech stack Prerequisite:**  HTML, CSS, JavaScript.
+
+## ****Project Steps****
+
+- **Step-1**: Set up the project
+    - Set up folder in which you have html , css and Javascript files that will contain code for your application.
+    - It may also contains some images which will be required in the application
+- **Step-2**: search functionality
+    - when any user type anything in the `input field`   after selecting a `category`  then the selected centre's card will render which justify that category type and match with the `input`.
+    - Suppose you want to search which centres of ISRO are there in the `state` Rajasthan  then you will click the button `state`  and type the name Rajasthan then it will show you all the centres in the states of Rajasthan .
+    - Make sure the search field is not case-sensitive . The output for search input `Rajasthan` , `rajasthan`  should be same.
+- **Step-3**: card rendering functionality
+    - When no category is selected, all the centre's card will render by default.
+    - When any category is selected and there is input in the search field, then only selected cards will render , like in the above given example only the cards which have their state Rajasthan will render .
+
+## Project Checkpoints
+
+- The search button
+- The city button
+- The state button
+- The centre button
